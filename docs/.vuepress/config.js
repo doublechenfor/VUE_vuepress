62620852312d792fd3a02fe91c16f58031e354bd
chenshuang4 <chenshuang4@vipkid.com.cn>
2020-01-15 23:01:17 +0800

    test:测试vuepress的lastupdated时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,41 +3,30 @@ module.exports = {
     description: "chenshuang vuepress",
     port: 9999,
     themeConfig: {
-        nav: [{
-            text: "Languages",
-            items: [{
-                    text: "Group1",
-                    items: [
-                        /*  */
-                    ]
-                },
-                {
-                    text: "Group2",
-                    items: [
-                        /*  */
-                    ]
-                }
-            ]
-        }],
-        sidebar: {
-            "/foo/": [
-                "" /* /foo/ */ ,
-                "one" /* /foo/one.html */ ,
-                "two" /* /foo/two.html */
+        search:true,  // 默认情况下为true
+        lastUpdated:true,  //最后一次git提交的时间 ,默认false
+        nav: [
+            {text:'Home',link:'/'},
+            {text:'github',link:'https://github.com/doublechenfor'},
+            {text:'代码风格',link:'https://github.com/doublechenfor'}
             ],
-
-            "/bar/": [
-                "" /* /bar/ */ ,
-                "three" /* /bar/three.html */ ,
-                "four" /* /bar/four.html */
-            ],
-
-            // fallback
-            "/": [
-                "" /* / */ ,
-                "contact" /* /contact.html */ ,
-                "about" /* /about.html */
-            ]
-        }
+        sidebar:  [
+            ['/guide/install','安装'],
+            ['/guide/introduction','介绍'],
+            ['/guide/test','测试demo'],  
+            {
+                title: 'UI组件',   // 必要的
+                path: '/guide/install',      // 可选的, 应该是一个绝对路径
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                  ['/guide/test','button'],
+                  ['/guide/test','icon'],
+                  ['/guide/test','images'],
+                  ['/guide/test','canvas']
+                ]
+              },          
+        ]
     }
+    
 };
